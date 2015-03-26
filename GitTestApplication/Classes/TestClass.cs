344bc0141dfb48aa8c062a15e0e97e80c2ed5fc7
lradael <l.radaelli@mdsnet.eu>
2015-03-26 14:35:03 +0100

    test2

---
+++ GitTestApplication/Classes/TestClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTestApplication.Classes
+{
+    public class TestClass
+    {
+        private int _count = 0;
+
+        public void Increase()
+        {
+            this._count++;
+        }
+
+        public void IncreaseByValue(int pValue)
+        {
+            this._count = this._count + pValue;
+        }
+    }
+}
